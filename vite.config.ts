4448395d0d17d6d399abcdd815df7ba44db1faf5
rockyrapanan <116909659+rockyrapanan@users.noreply.github.com>
2025-10-08 09:40:42 -0700

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ⬇️ replace REPO_NAME with your GitHub repo name
+  base: '/my-portfolio/'
 })
